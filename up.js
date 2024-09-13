aed016c808da876d4703c543d941554cd4aec2f3
Ivaaanq <aegis6467@gmail.com>
2024-09-13 11:11:14 +0500

    Add files via upload

---
+++ up.js
@@ -0,0 +1,19 @@
+let hour = 24
+let minut = 60
+let sec = 60
+let day = 30
+let month = 12
+let bite = 1024
+let kbite = 1024
+let mbite = 1024
+let gbite = 1024
+let dopbite = 10
+console.log(`в сутках ${hour*minut*sec} секунд `)
+console.log(`в месяце ${hour*minut*sec*day} секунд`)
+console.log(`в году ${hour*minut*sec*day*month} секунд`)
+console.log(`в сутках ${hour*minut} минут`)
+console.log(`в году ${hour*minut*day*month} минут`)
+console.log(`в мегабайте ${kbite*bite} байт`)
+console.log(`в гигабайте ${kbite*bite*mbite} байт`)
+console.log(`в 10 гигабайтах ${kbite*bite*mbite*dopbite} байт`)
+console.log(`в терабайте ${kbite*bite*mbite*gbite} байт`)
